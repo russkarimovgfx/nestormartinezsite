5a8a2b073fd85b1a17c1c8564bc7d94bdd37b9ad
Russ Karimov <rkarimov.gfx@gmail.com>
2021-09-17 12:25:16 -0400

    attempting to fix photo-gallery

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
 const hamburgerButton = document.getElementsByClassName('hamburger-button')[0];
 const menuButton = document.getElementsByClassName('menu-title')[0];
-const menuList = document.getElementsByClassName('menu')[0]
+const menuList = document.getElementsByClassName('menu')[0];
+const gallery = document.getElementById('gallery');
+const popUp = document.getElementById('popup');
+const selectedImage = document.getElementById('selectedImage');
+const imageIndexes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const selectedIndex = null;
 
 
 hamburgerButton.addEventListener('click', () => {
  menuButton.classList.toggle('menu-hidden')
  menuList.classList.toggle('menu-hidden')
  hamburgerButton.classList.toggle('ham-hidden')
+})
+
+
+// 
+
+imageIndexes.forEach(i => {
+ const image = document.createElement('img');
+ image.src = `/imagestwo/image${i}.jpg`;
+ image.classList.add('galleryImg');
+
+ image.addEventListener('click', () => {
+  popup.style.transform = `translateY(0)`;
+  selectedImage.src = `/imagestwo/image${i}.jpg`;
+ })
+
+ gallery.appendChild(image);
+})
+
+popup.addEventListener('click', () => {
+ popup.style.transform = `translateY(-100%)`
+ popup.src = '';
 })
